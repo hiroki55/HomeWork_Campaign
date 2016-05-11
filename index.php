57f36b25d716d902e73abb118740c6949b7ea9ff
itohiroki <hiroki.step.by.step@gmail.com>
2016-05-11 22:41:10 +0000

    no touch

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Donuts Campaign!</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="container">
+    <div id="title">
+      <h1>ドーナツが<br>もらえる<br>キャンペーン</h1>
+    </div>
+    <div class="top_btn">
+      <p>キャンペーンへ応募する from first</p>
+    <a href=""></a>
+    </div>
+  </div>
+</body>
+</html>
